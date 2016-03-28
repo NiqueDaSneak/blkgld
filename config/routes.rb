0357767807674503a307b57ce01b624f42dd624e
niquedasneak <niquedasneak4@gmail.com>
2016-03-28 16:19:27 -0400

    create welcome controller & associated files for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   get 'ratings/create'
 
   get 'ratings/show'
@@ -41,7 +43,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
