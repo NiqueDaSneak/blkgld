9019ec6d85eace2db12ad710b331f01b4ce1599e
niquedasneak <niquedasneak4@gmail.com>
2016-03-28 15:33:30 -0400

    Confirm database relationships are accurate, merging branch with master

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,12 @@
 
 Business.create(business_name: "The Yoga Bar", email: "email@email.com", password: "password", street_address: "15 W 14th Street", city: "Cincinnati", state: "OH", url: "www.theyogabars.com")
 
-User.create(username: "user1", email: "user1@email.com", password:"password")
+User.create(username: "user1", email: "user1@email.com", password: "password")
+User.create(username: "user2", email: "user2@email.com", password: "password")
+User.create(username: "user3", email: "user3@email.com", password: "password")
 
-greeting = "Hey there!"
+Comment.create(user_id: 1, business_id: 1, comment_text: "This place is fantastic!")
+
+Rating.create(user_id: 2, business_id: 1, value: 5)
+
+Favorite.create(user_id: 3, business_id: 1)
